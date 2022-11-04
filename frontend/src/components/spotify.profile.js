608fa7f88a942e1204a59b8327f9d51b6cfc8468
Joshua Samontanez <jsamontanez@knights.ucf.edu>
2022-11-04 16:46:39 -0400

    fix small screen bugs

--- frontend/src/components/spotify.profile.js
+++ frontend/src/components/spotify.profile.js
@@ -1,11 +1,13 @@
 import React from "react"
+import {NavLink} from 'react-router-dom'
 
 export default function Profile({ profile, numFollowing, playlist, topTracks, topArtists }) {
     if (!profile || !numFollowing || !playlist|| !topTracks || !topArtists ) return;
 
     const artists = topArtists.slice(0, 10).map((artist) => {
         return (
-            <article class="flex items-center gap-3 rounded-2xl bg-[#292f3d] mb-3 h-20">
+            <a href = {artist.external_urls.spotify} target="_blank" rel="noopener noreferrer"
+                class="flex items-center gap-3 rounded-2xl bg-[#292f3d] mb-3 h-20 hover:bg-[#3e4450] no-underline duration-200">
                 <span class="rounded-full ml-3">
                     <a href = {artist.external_urls.spotify} 
                             target="_blank" rel="noopener noreferrer"
@@ -17,17 +19,18 @@ export default function Profile({ profile, numFollowing, playlist, topTracks, to
                 <div class = "mt-2">
                     <a target="_blank" rel="noopener noreferrer" href = {artist.external_urls.spotify}
                         class = "no-underline hover:underline text-slate-50 hover:text-sky-300">
-                        <p class="lg:text-xl text-sm font-medium">{artist.name}</p>
+                        <p class="lg:text-xl text-md font-medium">{artist.name}</p>
                     </a>
 
                 </div>
-            </article>
+            </a>
         )
     })
 
     const tracks = topTracks.slice(0, 10).map((track) => {
         return (
-            <article class="flex items-center gap-3 rounded-2xl bg-[#292f3d] mb-3">
+            <a href = {track.external_urls.spotify} target="_blank" rel="noopener noreferrer"
+                class="flex items-center gap-3 rounded-2xl bg-[#292f3d] mb-3 hover:bg-[#3e4450] no-underline duration-200">
                 <span class="rounded-full ml-3">
                     <a href = {track.external_urls.spotify} 
                         target="_blank" rel="noopener noreferrer"
@@ -37,28 +40,33 @@ export default function Profile({ profile, numFollowing, playlist, topTracks, to
                     </a>
                 </span>
                 <div class = "mt-2">
-                    <p>
+                    <p class = "overflow-hidden truncate lg:w-60 w-48 text-slate-50 hover:text-sky-300"> 
                         <a target="_blank" rel="noopener noreferrer" 
-                            class="no-underline hover:underline 
-                            text-slate-50 hover:text-sky-300 lg:text-xl text-s font-medium" href = {track.external_urls.spotify}>
+                            class="no-underline hover:underline text-slate-50 hover:text-sky-300 
+                                lg:text-xl text-s font-medium" href = {track.external_urls.spotify}>
                                 {track.name}</a>
                     </p>
-                    <p class="lg:text-sm text-xs -mt-4 text-slate-300">{track.album.name}</p>
-                    <p class="lg:text-xs text-xs -mt-4 text-slate-400">{track.artists[0].name}</p>
+                    <p class="lg:text-sm text-xs -mt-4 text-slate-300 overflow-hidden truncate w-48">{track.album.name}</p>
+                    <p class="lg:text-xs text-xs -mt-4 text-slate-400 overflow-hidden truncate w-48">{track.artists[0].name}</p>
                 </div>
-            </article>
+            </a>
         )
     })
 
+    // Check if user have a profile picture, 
+    // if they don't the picture of their top artist will be set as their picture
+    const avatarImage = (profile.images.length === 0) ? 
+    `${topArtists[0].images[0].url}` : `${profile.images[0].url}`;
+
   return (
-    <div class = "ml-auto mr-auto lg:px-10 px-2">
+    <div class = "ml-auto mr-auto lg:px-10 px-2 pb-20">
         <div class="ml-auto mr-auto min-h-fit lg:w-3/6 flex items-center justify-center">
             <div class="p-6 rounded-xl w-full">
                 <img draggable="false" src={topTracks[0].album.images[0].url}
                     class="rounded-xl h-60 w-full object-cover shadow" alt=""/>
                 <div class="flex justify-center relative">
-                <img draggable="false" src={profile.images[0].url} 
-                    class="w-32 h-32 object-cover rounded-full border-8 border-[#111827] shadow absolute -top-16" alt=""/>
+                <img draggable="false" alt = "profile" src={avatarImage} 
+                    class="w-32 h-32 object-cover rounded-full border-8 border-[#111827] shadow absolute -top-16" />
                 </div>
                 <h1 class="text-slate-50 text-5xl mt-20 text-center font-bold mb-1 flex justify-center items-center space-x-2">
                     <a target="_blank" rel="noopener noreferrer" class="lg:text-5xl text-3xl font-bold no-underline hover:underline text-sky-50 hover:text-sky-300" href = {profile.external_urls.spotify} >{profile.display_name}</a>
@@ -89,13 +97,16 @@ export default function Profile({ profile, numFollowing, playlist, topTracks, to
                     </div>
                 </div>
                 </div>
-                <button class="hover:bg-slate-400 hover:text-[#292f3d] text-slate-400 
-                    border-4 border-slate-400 rounded-full shadow
-                    font-bold lg:py-3 lg:px-5 px-3 py-1
-                    relative mb-2 block transition-all 
-                    duration-300 ml-auto mr-auto lg:text-xl text-sm font-bold tracking-wider">
-                LOGOUT
-                </button>
+                <a href = "/landing" class = "no-underline">
+                    <button class="hover:bg-slate-400 hover:text-[#292f3d] text-slate-400 
+                        border-4 border-slate-400 rounded-full shadow
+                        font-bold lg:py-3 lg:px-5 px-3 py-1
+                        relative mb-2 block transition-all 
+                        duration-300 ml-auto mr-auto lg:text-xl text-sm font-bold tracking-wider">
+                    LOGOUT
+                    </button>
+                </a>
+
             </div>
         </div>
 
@@ -103,11 +114,13 @@ export default function Profile({ profile, numFollowing, playlist, topTracks, to
         <div class="grid lg:grid-flow-col grid-col-auto p-3 mr-auto ml-auto gap-2 max-w-screen-lg">
             <div class="col-span-2 w-full">
                 <div class = "flex -mb-4">
-                    <strong class = "mr-auto font-bold text-2xl p-3 text-white tracking-wide">Your Top Artists</strong>
-                    <div class = "p-3 ml-auto">
-                        <button class="hover:bg-slate-400 hover:text-[#292f3d] text-slate-400 font-bold lg:py-2 lg:px-3 py-1 px-2 border-2 
-                            border-slate-400 rounded-full shadow tracking-wide lg:text-sm text-xs">SEE MORE</button>
-                    </div>
+                    <strong class = "mr-auto font-bold text-2xl p-3 text-white tracking-wide">Your Top 10 Artists</strong>
+                    <NavLink exact to='/topartist'>
+                        <div class = "p-3 ml-auto">
+                            <button class="hover:bg-slate-400 hover:text-[#292f3d] text-slate-400 font-bold sm:py-2 sm:px-3 py-2 px-3 border-2 
+                                border-slate-400 rounded-full shadow tracking-wide lg:text-sm text-xs duration-300">SEE MORE</button>
+                        </div>
+                    </NavLink>
                 </div>
 
                 <div class="p-3">
@@ -117,13 +130,14 @@ export default function Profile({ profile, numFollowing, playlist, topTracks, to
 
             <div class="col-span-2 w-full">
                 <div class = "flex -mb-4">
-                    <strong class = "mr-auto font-bold text-2xl p-3 text-white tracking-wide">Your Top Tracks</strong>
-                    <div class = "p-3 ml-auto">
-                    <button class="hover:bg-slate-400 hover:text-[#292f3d] text-slate-400 font-bold lg:py-2 lg:px-3 py-1 px-2 border-2 
-                            border-slate-400 rounded-full shadow tracking-wide lg:text-sm text-xs">SEE MORE</button>
-                    </div>
+                    <strong class = "mr-auto font-bold text-2xl p-3 text-white tracking-wide">Your Top 10 Tracks</strong>
+                    <NavLink exact to='/toptracks'>
+                        <div class = "p-3 ml-auto">
+                            <button class="hover:bg-slate-400 hover:text-[#292f3d] text-slate-400 font-bold sm:py-2 sm:px-3 py-2 px-3 border-2 
+                                border-slate-400 rounded-full shadow tracking-wide lg:text-sm text-xs duration-300">SEE MORE</button>
+                        </div>
+                    </NavLink>
                 </div>
-
                 <div class="max-w-10/12 p-3">
                     {tracks}
                 </div>
